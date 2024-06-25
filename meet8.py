5bb3bff50b8e67e434e3e9d3aff0b41d2604e122
leonardoalexdavinci <145946853+leonardoalexdavinci@users.noreply.github.com>
2024-06-25 11:48:40 +0800

    meet8

---
+++ meet8.py
@@ -0,0 +1,14 @@
+# belajar percabangan
+
+nama = input ("Masukan Nama :")
+# if kondisi : aksi (standar)
+#if nama=="rian": print("Dosen Ganteng") 
+#print ("program selesai")
+
+# if kondisi : aksi (indentasi)
+if nama=="rian":
+    print("dosen ganteng")
+    print(f"ganteng banget, thanks {nama}")
+else:
+    print(f"keren banget, thanks {nama}")
+print ("program selesai")
